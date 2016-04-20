7c0a1c708bf95d1396c754d923d5919d4acc2e86
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2016-04-20 17:41:29 +0530

    Worked on deletion of node

--- binary_search_tree_del.cpp
+++ binary_search_tree_del.cpp
@@ -4,7 +4,7 @@ struct node
     int info;
     node *left;
     node *right;
-}*temp,*temp1,*root,*parent,*t;
+}*temp,*temp1,*root,*parent,*t,*pos;
 int dir_node;
 void inorder(node *temp)
 {
@@ -35,45 +35,11 @@ void postorder(node *temp)
     printf("%d ",temp->info);
 }
 
-void dele(node *pos)
-{
-   if(pos->left==NULL&&pos->right==NULL)
-   {
-       if(dir_node==1)
-       {
-           parent->left=NULL;
-           free(pos);
-       }
-       else
-       {
-           parent->right=NULL;
-           free(pos);
-       }
-   }
-   else if(pos->right=NULL)
-   {
-       pos->info=pos->left->info;
-       free(pos->left);
-   }
-   else if(pos->left=NULL)
-   {
-       pos->info=pos->right->info;
-       free(pos->right);
-   }
-   else
-   {
-       temp=pos->right;
-       while(temp->left!=NULL)
-       {
-           temp=temp->left;
-       }
-       pos->info=temp->info;
-       dele(temp);
-   }
-}
 node *sear(int val)
 {
-    temp=root;t=NULL;
+    temp=root;
+    t=NULL;
+    parent=root;
     while(temp!=NULL)
     {
         if(val<temp->info)
@@ -96,69 +62,119 @@ node *sear(int val)
     }
     return t;
 }
-main()
+
+void dele(int val)
 {
-int n;
-printf("Enter no.of elements to tree\n");
-scanf("%d",&n);
-printf("Enter %d elements to tree\n",n);
-for(int i=0;i<n;i++)
-{
-    temp=(node*)malloc(sizeof(node));
-    scanf("%d",&temp->info);
-    temp->left=NULL;
-    temp->right=NULL;
-    if(i==0)
-        root=temp;
-    else
+    pos=sear(val);
+
+    if(pos!=NULL)
     {
-        temp1=root;
-        while(1)
+        if(pos->left==NULL&&pos->right==NULL)
         {
-            if(temp->info>=temp1->info)
+            if(dir_node==1)
             {
-                if(temp1->right==NULL)
-                    break;
-                temp1=temp1->right;
+                parent->left=NULL;
+                free(pos);
             }
             else
             {
-                if(temp1->left==NULL)
-                    break;
-                temp1=temp1->left;
+                parent->right=NULL;
+                free(pos);
             }
         }
-        if(temp->info>=temp1->info)
-            temp1->right=temp;
+        else if(pos->right==NULL&&pos->left!=NULL)
+        {
+            if(dir_node==1)
+                temp=parent->left;
+            else
+                temp=parent->right;
+
+            if(dir_node==2)
+                parent->right=pos->left;
+            else
+                parent->left=pos->left;
+            free(temp);
+        }
+        else if(pos->left==NULL&&pos->right!=NULL)
+        {
+
+            if(dir_node==1)
+                temp=parent->left;
+            else
+                temp=parent->right;
+
+            if(dir_node==2)
+                parent->right=pos->right;
+            else
+                parent->left=pos->right;
+            free(temp);
+        }
         else
-            temp1->left=temp;
+        {
+            temp=pos->right;
+            node *save=temp;
+
+            if(temp->left==NULL)
+            {
+                pos->info=temp->info;
+                pos->right=NULL;
+                free(pos->right);
+            }
+            else
+            {
+                while(temp->left!=NULL)
+                {
+                    save=temp;
+                    temp=temp->left;
+                }
+                pos->info=temp->info;
+                save->left=NULL;
+                free(save->left);
+            }
+        }
     }
-}
-
-printf("\nPreorder is\n");
-    preorder(root);
-    printf("\n");
-
-    printf("\nInorder is\n");
-    inorder(root);
-    printf("\n");
-
-    printf("\nPostorder is\n");
-    postorder(root);
-    printf("\n");
-
-printf("\nEnter no.of elements to be deleted\n");
-int m;
-scanf("%d",&m);
-for(int i=0;i<m;i++)
-{
-    int val;
-    scanf("%d",&val);
-    node *pos=sear(val);
-    if(pos!=NULL)
-    dele(pos);
     else
         printf("\nElement to be deleted is not found\n");
+}
+
+main()
+{
+    int n;
+    printf("Enter no.of elements to tree\n");
+    scanf("%d",&n);
+    printf("Enter %d elements to tree\n",n);
+    for(int i=0; i<n; i++)
+    {
+        temp=(node*)malloc(sizeof(node));
+        scanf("%d",&temp->info);
+        temp->left=NULL;
+        temp->right=NULL;
+        if(i==0)
+            root=temp;
+        else
+        {
+            temp1=root;
+            while(1)
+            {
+                if(temp->info>=temp1->info)
+                {
+                    if(temp1->right==NULL)
+                        break;
+                    temp1=temp1->right;
+                }
+                else
+                {
+                    if(temp1->left==NULL)
+                        break;
+                    temp1=temp1->left;
+                }
+            }
+            if(temp->info>=temp1->info)
+                temp1->right=temp;
+            else
+                temp1->left=temp;
+        }
+    }
 
     printf("\nPreorder is\n");
     preorder(root);
@@ -171,5 +187,27 @@ for(int i=0;i<m;i++)
     printf("\nPostorder is\n");
     postorder(root);
     printf("\n");
-}
+
+    printf("\nEnter no.of elements to be deleted\n");
+    int m;
+    scanf("%d",&m);
+    for(int i=0; i<m; i++)
+    {
+        int val;
+        printf("\nEnter element to be deleted\n");
+        scanf("%d",&val);
+
+        dele(val);
+        printf("\nPreorder is\n");
+        preorder(root);
+        printf("\n");
+
+        printf("\nInorder is\n");
+        inorder(root);
+        printf("\n");
+
+        printf("\nPostorder is\n");
+        postorder(root);
+        printf("\n");
+    }
 }
